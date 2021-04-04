35f51fc1a7c08913aac3e6cc7819234cf7cfe045
Annie Wu <anniewu2303@gmail.com>
2021-04-03 22:49:37 -0700

    Initial commit, finished ui design

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// function toggleColor(buttonID) {
+//     var buttonPath = document.getElementById(buttonID).querySelectorAll(' svg path');
+
+//     for (var i = 0; i < buttonPath.clientHeight; i++) {
+//         if (button.style == '#fill: #808782') {
+//             buttonPath[i].setAttribute('style', 'fill: #CC7E85');
+//         }
+//         else {
+//             buttonPath[i].setAttribute('style', 'fill: #808782');
+//         }
+//     }
+// }
