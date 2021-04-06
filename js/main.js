391c58a6a229cad8938587a36515c1c6a31a7b00
Annie Wu <anniewu2303@gmail.com>
2021-04-06 15:36:14 -0700

    Added button color change on click and updated padding

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-// function toggleColor(buttonID) {
-//     var buttonPath = document.getElementById(buttonID).querySelectorAll(' svg path');
-
-//     for (var i = 0; i < buttonPath.clientHeight; i++) {
-//         if (button.style == '#fill: #808782') {
-//             buttonPath[i].setAttribute('style', 'fill: #CC7E85');
-//         }
-//         else {
-//             buttonPath[i].setAttribute('style', 'fill: #808782');
-//         }
-//     }
-// }
+function toggleColor(buttonId) {
+    document.getElementById(buttonId).classList.toggle('active')
+}
